ce39cfb4526c06a1911bebe2b6867b220264f049
Sophie <swing.sophie.8202@gmail.com>
2021-06-19 08:21:11 -0700

    HTML created form No JS/CSS

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!')
+//User types in a song name to retrieve the lyrics
+//Event Listener 1
+//  Form Submit - retrieves song lyrics
+//Event Listener 2
+//  Click to Minimize Song Lyrics to Song Title
+//Event Listener 3
+//  Click to Delete Song Title
+
+document.addEventListener('DOMContentLoaded', init)
+
